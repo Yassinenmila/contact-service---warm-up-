ecaca42602ec96ec0c4f4a21c291d5e2fdca62b9
yassinenmila <nmilayassine0000@gmail.com>
2026-08-27 22:50:37 +0100

    add the ajouter , afficher functions

--- main.py
+++ main.py
@@ -1,12 +1,49 @@
 contacts = []
 
+def sauvegarder_contacts():
+    with open("contacts.txt", "w") as fichier:
+        for contact in contacts:
+            fichier.write(
+                contact["nom"] + "|" +
+                contact["telephone"] + "|" +
+                contact["email"] + "\n"
+            )
 
 def ajouter_contact():
-    pass
+
+    nom = input("entrer le nom : ")
+    telephone = input("entrer le numero de telephone : ")
+    email = input("entrer l'email : ")
+
+    contact= {
+        "nom"= nom,
+        "telephone"=telephone,
+        "email"=email
+    }
+        
+    contacts.append(contact)
+
+    sauvegarder_contacts()
+
+    print("contact ajouter !!!!!!")
 
 
 def afficher_contacts():
-    pass
+    try:
+        with open("contacts.txt", "r") as fichier:
+            contacts = fichier.readlines()
+
+        if not contacts:
+            print("Aucun contact.")
+            return
+
+        for i, ligne in enumerate(contacts, start=1):
+            nom, telephone, email = ligne.strip().split("|")
+
+            print(f"{i}. {nom} | {telephone} | {email}")
+
+    except FileNotFoundError:
+        print("Aucun contact.")
 
 
 def supprimer_contact():
@@ -24,11 +61,11 @@ while True:
 
     match choix:
         case "1":
-            print("ajouter un contact ")
+            ajouter_contact()
         case "2":
-            print("afficher un contact ")
+            afficher_contacts()
         case "3":
-            print("suprimer un contact ")
+            supprimer_contact()
         case "4":
             break
         case _:
