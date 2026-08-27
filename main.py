cb578731febeb14512f780da478e839946149c51
yassinenmila <nmilayassine0000@gmail.com>
2026-08-27 22:27:20 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+while True:
+    print("========== MENU ==========")
+    print("1. Ajouter un contact ")
+    print("2. Afficher les contacts ")
+    print("3. Suprimer un contact")
+    print("4. Quitter")
+
+    choix = input("Votre choix : ")
+
+    match choix:
+        case "1":
+            print("ajouter un contact ")
+        case "2":
+            print("afficher un contact ")
+        case "3":
+            print("suprimer un contact ")
+        case "4":
+            break
+        case _:
+            print("choix invalide")
+    
